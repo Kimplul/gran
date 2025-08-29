50a9b6780e04ae27cd03323d7fec02e15f7d3086
Kimplul <kimi.h.kuparinen@gmail.com>
2025-08-29 22:39:10 +0300

    remove starved mesh test
    + Essentially equivalent to node2d test, just worse

--- tests/starved_mesh/source.mk
+++
@@ -1,9 +0,0 @@
-STARVED_MESH		:= tests/starved_mesh
-STARVED_MESH_SIM	:= $(STARVED_MESH)/sim.c
-
-TESTS += $(STARVED_MESH)/sim
-
-$(STARVED_MESH)/sim: $(STARVED_MESH_SIM) libgran.a
-	mkdir -p build/$(STARVED_MESH)
-	$(COMPILE_TEST) $(STARVED_MESH_SIM) libgran.a -o build/$@
-	./scripts/gen-report -d build $@
